aff2751e1128f820077e17d7dc3c8ee2f07183ef
bwginfo <70068818+bwginfo@users.noreply.github.com>
2025-03-25 21:27:15 +0200

    Update LiquidCrystal_PCF8574_NuMaker.ino

--- LiquidCrystal_PCF8574_NuMaker.ino
+++ LiquidCrystal_PCF8574_NuMaker.ino
@@ -1,4 +1,5 @@
 // This example shows various featues of the library for LCD with 16 chars and 2 lines.
+// This example shows various featues of the library for LCD with 20 chars and 4 lines.
 // NuMaker UNO nuc131 / Arduino 1.8.15
 // library LiquidCrystal_PCF8574 at version 2.2.0 in
 // nuvoton\hardware\nuvoton\2.3.4\libraries\Wire
